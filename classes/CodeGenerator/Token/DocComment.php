a20049ad58f8e7314bc3c749914ce694c88dfbf4
Korney Czukowski <carbofos@seznam.cz>
2012-07-25 17:47:59 +0200

    Added Doc comment token class

---
+++ classes/CodeGenerator/Token/DocComment.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * DocComment class
+ * 
+ * @package    CodeGenerator
+ * @category   Tokens
+ * @author     Korney Czukowski
+ * @copyright  (c) 2012 Korney Czukowski
+ * @license    MIT License
+ */
+namespace CodeGenerator\Token;
+
+class DocComment extends Token
+{
+	protected $attributes = array(
+		'annotations' => array(),
+		'text' => NULL,
+	);
+
+	public function render()
+	{
+		if ( ! $this->attributes['annotations'] AND ! $this->attributes['text'])
+		{
+			return '';
+		}
+		$this->config->helper('columnsOptimizer')
+			->tokens($this->attributes['annotations'])
+			->align();
+		$lines = array('/**');
+		if ($this->attributes['text'])
+		{
+			$lines[] = ' * '.$this->attributes['text'];
+		}
+		foreach ($this->attributes['annotations'] as $annotation)
+		{
+			$lines[] = ' * '.$annotation;
+		}
+		$lines[] = ' */';
+		return implode("\n", $lines);
+	}
+
+}

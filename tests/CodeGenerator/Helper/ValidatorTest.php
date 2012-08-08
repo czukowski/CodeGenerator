5efaf89a96ce905c4ea48dc14166cb369d35197b
Korney Czukowski <carbofos@seznam.cz>
2012-08-08 06:52:39 +0200

    Reordered alphabetically

--- tests/CodeGenerator/Helper/ValidatorTest.php
+++ tests/CodeGenerator/Helper/ValidatorTest.php
@@ -10,33 +10,7 @@
 namespace CodeGenerator\Helper;
 
 class ValidatorTest extends Testcase
-{
-	/**
-	 * @dataProvider  provide_name
-	 */
-	public function test_validate_name($name, $expected)
-	{
-		$this->setup_object();
-		$this->assertEquals($expected, $this->object->validate_name($name));
-	}
-
-	public function provide_name()
-	{
-		return array(
-			array(NULL, FALSE),
-			array(TRUE, FALSE),
-			array(FALSE, FALSE),
-			array(0, FALSE),
-			array('123', FALSE),
-			array('a123', TRUE),
-			array('a_bC', TRUE),
-			array('a_b-C', FALSE),
-			array('a_b0C', TRUE),
-			array('var', TRUE),
-			array('$var', FALSE),
-		);
-	}
-
+{	
 	/**
 	 * @dataProvider  provide_constraint
 	 */
@@ -62,6 +36,32 @@ class ValidatorTest extends Testcase
 			array('\\\\Classname', FALSE),
 			array('\\Classname\\Subname', TRUE),
 			array('\\Classname\\$ubname', FALSE),
+		);
+	}
+
+	/**
+	 * @dataProvider  provide_name
+	 */
+	public function test_validate_name($name, $expected)
+	{
+		$this->setup_object();
+		$this->assertEquals($expected, $this->object->validate_name($name));
+	}
+
+	public function provide_name()
+	{
+		return array(
+			array(NULL, FALSE),
+			array(TRUE, FALSE),
+			array(FALSE, FALSE),
+			array(0, FALSE),
+			array('123', FALSE),
+			array('a123', TRUE),
+			array('a_bC', TRUE),
+			array('a_b-C', FALSE),
+			array('a_b0C', TRUE),
+			array('var', TRUE),
+			array('$var', FALSE),
 		);
 	}
 }

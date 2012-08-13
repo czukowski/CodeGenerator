5875614683cc90728641d59b57d1d60deda3146b
Korney Czukowski <carbofos@seznam.cz>
2012-08-13 07:06:43 +0200

    Added block render method

--- classes/CodeGenerator/Token/Block.php
+++ classes/CodeGenerator/Token/Block.php
@@ -43,4 +43,32 @@ abstract class Block extends Token
 		}
 		throw new \InvalidArgumentException('Indentation must be integer');
 	}
+
+	/**
+	 * Renders block of lines and indents them
+	 * 
+	 * @param  array  $lines
+	 */
+	protected function render_block($lines)
+	{
+		if ( ! $this->config->helper('arrays')->is_array($lines))
+		{
+			throw new \InvalidArgumentException($this->get_type().'.render_block() takes an array as argument');
+		}
+		foreach ($lines as &$line)
+		{
+			$line = $this->render_line($line);
+		}
+		return implode($this->config->get_format('line_end'), $lines);
+	}
+
+	/**
+	 * Renders a single line or token
+	 */
+	private function render_line($line)
+	{
+		$line_end = $this->config->get_format('line_end');
+		$indentation = str_repeat($this->config->get_format('indent'), $this->get_indentation());
+		return $indentation.str_replace($line_end, $line_end.$indentation, $line);
+	}
 }
--- tests/CodeGenerator/Token/BlockTest.php
+++ tests/CodeGenerator/Token/BlockTest.php
@@ -34,4 +34,46 @@ class BlockTest extends Testcase
 			array('', new \InvalidArgumentException),
 		);
 	}
+
+	/**
+	 * @dataProvider  provide_render_block
+	 */
+	public function test_render_block($lines, $indent, $expected)
+	{
+		$this->setup_mock();
+		$this->set_expected_exception_from_argument($expected);
+		$this->object->set_indentation($indent);
+		$actual = $this->get_object_method($this->object, 'render_block')
+			->invoke($this->object, $lines);
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function provide_render_block()
+	{
+		return array(
+			array(
+				array(
+					'$a = 1;',
+					'$b = $a;',
+				),
+				2,
+				"\t\t\$a = 1;\n".
+				"\t\t\$b = \$a;",
+			),
+			array(
+				array(
+					"\$a = 1;\n\$b = \$a;",
+				),
+				2,
+				"\t\t\$a = 1;\n".
+				"\t\t\$b = \$a;",
+			),
+			array(
+				'Weird argument', 0, new \InvalidArgumentException,
+			),
+			array(
+				new \stdClass(), 10, new \InvalidArgumentException,
+			),
+		);
+	}
 }

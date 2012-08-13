d62e2668b3bfa58081d9af1e8a7f3cffe013a72a
Korney Czukowski <carbofos@seznam.cz>
2012-08-13 16:52:12 +0200

    Added doc comments

--- classes/CodeGenerator/Helper/Validator.php
+++ classes/CodeGenerator/Helper/Validator.php
@@ -13,16 +13,25 @@ class Validator extends \CodeGenerator\Singleton
 {
 	const NAME_PATTERN = '[A-Za-z_][A-Za-z0-9_]+?';
 
+	/**
+	 * Tests that the value is a string that can be used as a class name
+	 */
 	public function validate_constraint($value)
 	{
 		return is_string($value) AND preg_match('#^(?:\\\\?'.self::NAME_PATTERN.')+$#', $value);
 	}
 
+	/**
+	 * Tests that the value is integer
+	 */
 	public function validate_integer($value)
 	{
 		return is_int($value);
 	}
 
+	/**
+	 * Tests that the value is a string that can be used as a variable name
+	 */
 	public function validate_name($value)
 	{
 		return is_string($value) AND preg_match('#^'.self::NAME_PATTERN.'$#', $value);

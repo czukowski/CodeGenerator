24acb8f67446c9ab84e89ff74bc2f3ec5f596876
Korney Czukowski <carbofos@seznam.cz>
2012-08-15 17:49:59 +0200

    Added Type class

---
+++ classes/CodeGenerator/Token/Type.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Type class that represents Class tokens
+ * 
+ * @package    CodeGenerator
+ * @category   Tokens
+ * @author     Korney Czukowski
+ * @copyright  (c) 2012 Korney Czukowski
+ * @license    MIT License
+ */
+namespace CodeGenerator\Token;
+
+class Type extends Block
+{
+	protected function initialize()
+	{
+		parent::initialize();
+		$this->initialize_attributes(array(
+			'comment' => NULL,
+			'type' => 'class',
+			'abstract' => NULL,
+			'final' => NULL,
+			'namespace' => NULL,
+			'use' => array(),
+			'name' => NULL,
+			'extends' => NULL,
+			'implements' => array(),
+			'traits' => array(),
+			'body' => array(),
+			'indentation' => 1,
+		));
+		$this->initialize_validation(array(
+			'type' => 'type',
+		));
+	}
+
+	public function render()
+	{
+		if ( ! $this->get('name') OR ! $this->get('type'))
+		{
+			return '';
+		}
+		$lines = array(
+			$this->render_heading(),
+			$this->render_body(),
+			$this->render_footing(),
+		);
+		$glue = $this->get('body') ? $this->config->get_format('line_end') : '';
+		return implode($glue, $lines);
+	}
+
+	private function render_heading()
+	{
+		$heading = implode($this->get_items_glue(), array_filter(array(
+			$this->render_comment(),
+			$this->render_namespace(),
+			$this->render_declaration(),
+		)));
+		$heading .= $this->config->get_format('brace_open');
+		return $heading;
+	}
+
+	private function render_comment()
+	{
+		return $this->render_block_comment($this->get('comment'));
+	}
+
+	private function render_namespace()
+	{
+		return implode($this->config->get_format('line_end'), array_filter(array(
+			($namespace = $this->get('namespace')) ? 'namespace '.$namespace : '',
+			($use = $this->get('use')) ? 'use '.implode($this->config->get_format('line_end'), $use) : '',
+		)));
+	}
+
+	private function render_declaration()
+	{
+		return implode(' ', array_filter(array(
+			$this->render_boolean_attribute('abstract'),
+			$this->render_boolean_attribute('final'),
+			$this->get('type'),
+			$this->get('name'),
+			$this->render_extends(),
+			$this->render_implements(),
+		)));
+	}
+
+	private function render_extends()
+	{
+		if (($classname = $this->get('extends')))
+		{
+			return 'extends '.$classname;
+		}
+	}
+
+	private function render_implements()
+	{
+		if (($interfaces = $this->get('implements')))
+		{
+			return 'implements '.implode(', ', $interfaces);
+		}
+	}
+
+	private function render_body()
+	{
+		return $this->render_block($this->get('body'), $this->get_items_glue());
+	}
+
+	private function render_footing()
+	{
+		return $this->config->get_format('brace_close');
+	}
+
+	private function get_items_glue()
+	{
+		return str_repeat($this->config->get_format('line_end'), 2);
+	}
+
+	public function validate_type($value)
+	{
+		return in_array($value, array('class', 'interface'), TRUE);
+	}
+}
---
+++ tests/CodeGenerator/Token/TypeTest.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * TypeTest
+ * 
+ * @package    CodeGenerator
+ * @category   Tokens
+ * @author     Korney Czukowski
+ * @copyright  (c) 2012 Korney Czukowski
+ * @license    MIT License
+ */
+namespace CodeGenerator\Token;
+
+class TypeTest extends Testcase
+{
+	/**
+	 * @dataProvider  provide_render
+	 */
+	public function test_render($attributes, $expected)
+	{
+		$this->setup_with_attributes($attributes);
+		$this->assertEquals($expected, $this->object->render());
+	}
+
+	public function provide_render()
+	{
+		return array(
+			// Name attribute not set
+			array(
+				array(
+					'type' => 'class',
+				),
+				'',
+			),
+			// Interface w/o body
+			array(
+				array(
+					'type' => 'interface',
+					'name' => 'IDoSomething',
+				),
+				"interface IDoSomething\n".
+				"{}",
+			),
+			// Basic class w/extends and implements
+			array(
+				array(
+					'type' => 'class',
+					'name' => 'Vehicle',
+					'extends' => 'Tech',
+					'implements' => array('IDoSomething', 'IMove'),
+				),
+				"class Vehicle extends Tech implements IDoSomething, IMove\n".
+				"{}",
+			),
+			// Final class
+			array(
+				array(
+					'type' => 'class',
+					'name' => 'HTTPRequest',
+					'final' => TRUE,
+					'implements' => array('IRequest'),
+					'body' => array('// Here go class methods'),
+				),
+				"final class HTTPRequest implements IRequest\n".
+				"{\n".
+				"\t// Here go class methods\n".
+				"}",
+			),
+			// Class with methods and properties
+			array(
+				array(
+					'type' => 'class',
+					'name' => 'Foo',
+					'body' => array(
+						'private $bar;',
+						$this->create_method('get_bar', array(), array('return $this->bar;')),
+						$this->create_method('set_bar', array('$value'), array('$this->bar = $value;')),
+					),
+				),
+				"class Foo\n".
+				"{\n".
+				"\tprivate \$bar;\n".
+				"\n".
+				"\tpublic function get_bar()\n".
+				"\t{\n".
+				"\t\treturn \$this->bar;\n".
+				"\t}\n".
+				"\n".
+				"\tpublic function set_bar(\$value)\n".
+				"\t{\n".
+				"\t\t\$this->bar = \$value;\n".
+				"\t}\n".
+				"}",
+			),
+			// Namespace and doc comments options
+			array(
+				array(
+					'comment' => "@package Fubar",
+					'type' => 'interface',
+					'name' => 'ISuperObject',
+					'namespace' => '\Foo',
+					'use' => array('\Foo\Bar as Bar'),
+					'body' => array('// Here go class methods'),
+				),
+				"/**\n".
+				" * @package Fubar\n".
+				" */\n".
+				"\n".
+				"namespace \Foo\n".
+				"use \Foo\Bar as Bar\n".
+				"\n".
+				"interface ISuperObject\n".
+				"{\n".
+				"\t// Here go class methods\n".
+				"}",
+			),
+		);
+	}
+
+	private function create_method($name, $arguments, $body)
+	{
+		return $this->get_config()
+			->helper('tokenFactory')
+			->create('Method', array(
+				'name' => $name,
+				'access' => 'public',
+				'arguments' => $arguments,
+				'body' => $body,
+			));
+	}
+
+	/**
+	 * @dataProvider  provide_validate_type
+	 */
+	public function test_validate_type($value, $expected)
+	{
+		$this->setup_object();
+		$this->assertEquals($expected, $this->object->validate_type($value));
+	}
+
+	public function provide_validate_type()
+	{
+		return array(
+			array('class', TRUE),
+			array('interface', TRUE),
+			array('something else', FALSE),
+			array(7.62, FALSE),
+			array(TRUE, FALSE),
+			array(FALSE, FALSE),
+			array(NULL, FALSE),
+			array(new \stdClass, FALSE),
+		);
+	}
+}

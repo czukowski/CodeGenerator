e29e8dcfbd4d76ca1c1b7ffdf60764f1bee7e3b8
Korney Czukowski <carbofos@seznam.cz>
2012-09-05 07:50:27 +0200

    Added language construct token

---
+++ classes/CodeGenerator/Token/Construct.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Language construct token (if, foreach, etc)
+ * 
+ * @package    CodeGenerator
+ * @category   Tokens
+ * @author     Korney Czukowski
+ * @copyright  (c) 2012 Korney Czukowski
+ * @license    MIT License
+ */
+namespace CodeGenerator\Token;
+
+class Construct extends Token
+{
+	protected function initialize()
+	{
+		parent::initialize();
+		$this->initialize_attributes(array(
+			'type' => NULL,
+			'condition' => NULL,
+			'body' => array(),
+		));
+		$this->initialize_validation(array(
+			'type' => 'type',
+		));
+	}
+
+	public function render()
+	{
+		if ( ! $this->get('type'))
+		{
+			return '';
+		}
+		$body_render = $this->render_body();
+		$lines = array(
+			$this->render_heading(),
+			$body_render,
+			$this->render_footing(),
+		);
+		$glue = $body_render ? $this->config->get_format('line_end') : '';
+		return implode($glue, $lines);
+	}
+
+	private function render_heading()
+	{
+		$line = $this->get('type');
+		if ($this->is_condition_in_heading($line))
+		{
+			$line .= $this->render_condition();
+		}
+		$line .= $this->config->get_format('brace_open');
+		return $line;
+	}
+
+	private function is_condition_in_heading($type)
+	{
+		return ! in_array($type, array('else', 'do'));
+	}
+
+	private function render_body()
+	{
+		if (($body = $this->get('body')))
+		{
+			return (string) $this->config->helper('tokenFactory')
+				->transform('Block', $body, $this);
+		}
+	}
+
+	private function render_footing()
+	{
+		$line = $this->config->get_format('brace_close');
+		if ($this->get('type') === 'do')
+		{
+			$line .= $this->config->get_format('line_end');
+			$line .= 'while'.$this->render_condition().';';
+		}
+		return $line;
+	}
+
+	private function render_condition()
+	{
+		return ' ('.$this->get('condition').')';
+	}
+
+	public function validate_type($value)
+	{
+		return in_array($value, array('if', 'else', 'elseif', 'for', 'foreach', 'switch', 'while', 'do'), TRUE);
+	}
+}
---
+++ tests/CodeGenerator/Token/ConstructTest.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * ConstructTest
+ * 
+ * @package    CodeGenerator
+ * @category   Tokens
+ * @author     Korney Czukowski
+ * @copyright  (c) 2012 Korney Czukowski
+ * @license    MIT License
+ */
+namespace CodeGenerator\Token;
+
+class ConstructTest extends Testcase
+{
+	/**
+	 * @dataProvider  provide_render
+	 */
+	public function test_render($attributes, $expected)
+	{
+		$this->setup_with_attributes($attributes);
+		$this->assertEquals($expected, $this->object->render());
+	}
+
+	public function provide_render()
+	{
+		return array(
+			// Type attribute not set
+			array(
+				array(
+					'body' => 'return TRUE;',
+				),
+				'',
+			),
+			// Body attribute not set
+			array(
+				array(
+					'type' => 'if',
+					'condition' => 'TRUE',
+				),
+				"if (TRUE)\n".
+				"{}",
+			),
+			// Else with condition (ignored)
+			array(
+				array(
+					'type' => 'else',
+					'condition' => '$value === FALSE',
+					'body' => 'return TRUE;',
+				),
+				"else\n".
+				"{\n".
+				"\treturn TRUE;\n".
+				"}",
+			),
+			// If
+			array(
+				array(
+					'type' => 'if',
+					'condition' => '$model->is_valid()',
+					'body' => array(
+						'$model->save();',
+						'return;',
+					),
+				),
+				"if (\$model->is_valid())\n".
+				"{\n".
+				"\t\$model->save();\n".
+				"\treturn;\n".
+				"}",
+			),
+			// While
+			array(
+				array(
+					'type' => 'while',
+					'condition' => 'TRUE',
+					'body' => array(
+						'$this->loop();',
+					),
+				),
+				"while (TRUE)\n".
+				"{\n".
+				"\t\$this->loop();\n".
+				"}",
+			),
+			// Do (special case)
+			array(
+				array(
+					'type' => 'do',
+					'condition' => 'TRUE',
+					'body' => array(
+						'$this->loop();',
+					),
+				),
+				"do\n".
+				"{\n".
+				"\t\$this->loop();\n".
+				"}\n".
+				"while (TRUE);",
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider  provide_is_condition_in_heading
+	 */
+	public function test_is_condition_in_heading($value, $expected)
+	{
+		$this->setup_object();
+		$actual = $this->get_object_method($this->object, 'is_condition_in_heading')
+			->invoke($this->object, $value);
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function provide_is_condition_in_heading()
+	{
+		return array(
+			array('if', TRUE),
+			array('elseif', TRUE),
+			array('else', FALSE),
+			array('for', TRUE),
+			array('foreach', TRUE),
+			array('while', TRUE),
+			array('do', FALSE),
+			array('switch', TRUE),
+		);
+	}
+
+	/**
+	 * @dataProvider  provide_validate_type
+	 */
+	public function test_validate_type($value, $expected)
+	{
+		$this->setup_object();
+		$this->assertEquals($expected, $this->object->validate_type($value));
+	}
+
+	public function provide_validate_type()
+	{
+		return array(
+			array('if', TRUE),
+			array('elseif', TRUE),
+			array('else', TRUE),
+			array('for', TRUE),
+			array('foreach', TRUE),
+			array('while', TRUE),
+			array('do', TRUE),
+			array('switch', TRUE),
+			array('something else', FALSE),
+			array(7.62, FALSE),
+			array(TRUE, FALSE),
+			array(FALSE, FALSE),
+			array(NULL, FALSE),
+			array(new \stdClass, FALSE),
+		);
+	}
+
+}

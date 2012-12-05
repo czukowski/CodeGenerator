c48f6ffe2cf6763b3b97f5c504115059b3033508
Korney Czukowski <carbofos@seznam.cz>
2012-12-05 07:37:52 +0100

    Added explicit format option

--- classes/CodeGenerator/Helper/TokenPartsRenderer.php
+++ classes/CodeGenerator/Helper/TokenPartsRenderer.php
@@ -15,15 +15,20 @@ class TokenPartsRenderer extends \CodeGenerator\Singleton
 	 * Renders a class name in either camel case or underscore form, depending on
 	 * the config options. Capital case is used for each word.
 	 * 
-	 * @param   string  $str  phrase to format
+	 * @param   string  $str     phrase to format
+	 * @param   string  $option  camelcalse or underscore
 	 * @return  string
 	 */
-	public function render_class_name($str)
+	public function render_class_name($str, $option = NULL)
 	{
+		if ($option === NULL)
+		{
+			$option = $this->config->get_options('names');
+		}
 		$namespace_parts = explode('\\', $str);
 		foreach ($namespace_parts as &$part)
 		{
-			$part = ucfirst($this->render_name(ucwords($part)));
+			$part = ucfirst($this->render_name(ucwords($part), $option));
 		}
 		return implode('\\', $namespace_parts);
 	}
@@ -32,18 +37,23 @@ class TokenPartsRenderer extends \CodeGenerator\Singleton
 	 * Renders a phrase in either camel case or underscore form, depending on
 	 * the config options.
 	 * 
-	 * @param   string  $str  phrase to format
+	 * @param   string  $str     phrase to format
+	 * @param   string  $option  camelcalse or underscore
 	 * @return  string
 	 */
-	public function render_name($str)
+	public function render_name($str, $option = NULL)
 	{
 		// If the string is not a phrase, return the original string
 		if (strpos($str, ' ') === FALSE)
 		{
 			return $str;
 		}
+		if ($option === NULL)
+		{
+			$option = $this->config->get_options('names');
+		}
 		// Otherwise return formatted string appropriately to the config option
-		switch ($this->config->get_options('names'))
+		switch ($option)
 		{
 			case 'camelcase':
 				return $this->config->helper('inflector')
@@ -55,6 +65,6 @@ class TokenPartsRenderer extends \CodeGenerator\Singleton
 				break;
 		}
 		// Break on invalid config option
-		throw new \LogicException('Invalid config option `names`');
+		throw new \LogicException('Invalid format option: "'.$option.'"');
 	}
 }
--- tests/CodeGenerator/Helper/TokenPartsRendererTest.php
+++ tests/CodeGenerator/Helper/TokenPartsRendererTest.php
@@ -14,13 +14,13 @@ class TokenPartsRendererTest extends Testcase
 	/**
 	 * @dataProvider  provide_render_class_name
 	 */
-	public function test_render_class_name($format, $name, $expected)
+	public function test_render_class_name($format, $name, $option, $expected)
 	{
 		$this->setup_object(array(
 			'arguments' => array($this->create_config($format)),
 		));
 		$this->set_expected_exception_from_argument($expected);
-		$actual = $this->object->render_class_name($name);
+		$actual = $this->object->render_class_name($name, $option);
 		$this->assertSame($expected, $actual);
 	}
 
@@ -28,31 +28,38 @@ class TokenPartsRendererTest extends Testcase
 	{
 		return array(
 			// Phrase names
-			array('camelcase', 'some class name', 'SomeClassName'),
-			array('camelcase', 'Some Class Name', 'SomeClassName'),
-			array('underscore', 'some class name', 'Some_Class_Name'),
-			array('underscore', 'Some Class Name', 'Some_Class_Name'),
+			array('camelcase', 'some class name', NULL, 'SomeClassName'),
+			array('camelcase', 'Some Class Name', NULL, 'SomeClassName'),
+			array('underscore', 'some class name', NULL, 'Some_Class_Name'),
+			array('underscore', 'Some Class Name', NULL, 'Some_Class_Name'),
+			// Explicit format option
+			array('camelcase', 'some class name', 'underscore', 'Some_Class_Name'),
+			array('camelcase', 'Some Class Name', 'underscore', 'Some_Class_Name'),
+			array('underscore', 'some class name', 'camelcase', 'SomeClassName'),
+			array('underscore', 'Some Class Name', 'camelcase', 'SomeClassName'),
 			// Class names with namespaces
-			array('camelcase', 'Namespace\Some Class Name', 'Namespace\SomeClassName'),
-			array('underscore', 'Namespace\some class name', 'Namespace\Some_Class_Name'),
+			array('camelcase', 'Namespace\Some Class Name', NULL, 'Namespace\SomeClassName'),
+			array('camelcase', 'Namespace\Some Class Name', 'underscore', 'Namespace\Some_Class_Name'),
+			array('underscore', 'Namespace\some class name', NULL, 'Namespace\Some_Class_Name'),
+			array('underscore', 'Namespace\some class name', 'camelcase', 'Namespace\SomeClassName'),
 			// Already formatted names will not be changed
-			array('camelcase', 'Some_Class_Name', 'Some_Class_Name'),
-			array('underscore', 'SomeClassName', 'SomeClassName'),
+			array('camelcase', 'Some_Class_Name', NULL, 'Some_Class_Name'),
+			array('underscore', 'SomeClassName', NULL, 'SomeClassName'),
 			// Invalid config option
-			array('anything else', 'Some Class Name', new \LogicException),
+			array('anything else', 'Some Class Name', NULL, new \LogicException),
 		);
 	}
 
 	/**
 	 * @dataProvider  provide_render_name
 	 */
-	public function test_render_name($format, $name, $expected)
+	public function test_render_name($format, $name, $option, $expected)
 	{
 		$this->setup_object(array(
 			'arguments' => array($this->create_config($format)),
 		));
 		$this->set_expected_exception_from_argument($expected);
-		$actual = $this->object->render_name($name);
+		$actual = $this->object->render_name($name, $option);
 		$this->assertSame($expected, $actual);
 	}
 
@@ -60,15 +67,20 @@ class TokenPartsRendererTest extends Testcase
 	{
 		return array(
 			// Phrase names
-			array('camelcase', 'some variable name', 'someVariableName'),
-			array('camelcase', 'Some Variable Name', 'someVariableName'),
-			array('underscore', 'some variable name', 'some_variable_name'),
-			array('underscore', 'Some Variable Name', 'Some_Variable_Name'),
+			array('camelcase', 'some variable name', NULL, 'someVariableName'),
+			array('camelcase', 'Some Variable Name', NULL, 'someVariableName'),
+			array('underscore', 'some variable name', NULL, 'some_variable_name'),
+			array('underscore', 'Some Variable Name', NULL, 'Some_Variable_Name'),
+			// Explicit format option
+			array('camelcase', 'some variable name', 'underscore', 'some_variable_name'),
+			array('camelcase', 'Some Variable Name', 'underscore', 'Some_Variable_Name'),
+			array('underscore', 'some variable name', 'camelcase', 'someVariableName'),
+			array('underscore', 'Some Variable Name', 'camelcase', 'someVariableName'),
 			// Already formatted names will not be changed
-			array('camelcase', 'Some_Class_Name', 'Some_Class_Name'),
-			array('underscore', 'SomeClassName', 'SomeClassName'),
+			array('camelcase', 'Some_Class_Name', NULL, 'Some_Class_Name'),
+			array('underscore', 'SomeClassName', NULL, 'SomeClassName'),
 			// Invalid config option
-			array('anything else', 'Some Variable Name', new \LogicException),
+			array('anything else', 'Some Variable Name', NULL, new \LogicException),
 		);
 	}
 
